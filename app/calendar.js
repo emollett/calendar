bcf772e804c084723250973dbeafaaa7d3d4ad55
Eleanor Mollett <eleanor.mollett@gmail.com>
2018-09-05 15:40:59 +0100

    Updated css to make the boxes sit in the right place. Started work on duplicating the calendar block so that it appears for the number of weeks entered. Currently not working, but is duplicating and putting one duplicate in the right place.

--- app/calendar.js
+++ app/calendar.js
@@ -9,3 +9,22 @@
       sunday.style.visibility="visible";
       }
     }
+
+
+function calendarCreate(){
+  //to make the calendar appear with the right number of weeks repeated
+  var numberOfWeeks = document.getElementById("noWeeks");
+  var block = document.getElementById("calBlock");
+  var blockExtend = block.cloneNode(true);
+
+  for (i=0; i<numberOfWeeks.value; i++){
+    //boxy bit for testing
+    document.getElementById("cal").appendChild(blockExtend);
+    //texty bit for testing
+    //var node = document.createElement("P");
+    //var textnode = document.createTextNode("Hellooo " + numberOfWeeks.value);
+    //node.appendChild(textnode);
+    //document.getElementById("cal").appendChild(node);
+  }
+
+}
