8342d181201fd37db5f94ec8f05d6039aa226e31
Eleanor Mollett <eleanor.mollett@gmail.com>
2018-09-20 13:52:33 +0100

    The weekend toggle is now working again by using adding a class to specific boxes of hidden. I'd like to tidy up the if statements in the future as they are a bit messy. Next up is looking at the time bug - potentially using moment. I also want to get the starting date into the right box (currently whatever date it is starts in the first box, which should only be if it is a Monday.)

--- app/calendar.js
+++ app/calendar.js
@@ -1,38 +1,55 @@
-function hide(){
-  var checkBox = document.getElementById("weekendYes");
-
-  if (checkBox.checked == false){
-    saturday.style.visibility="hidden";
-    sunday.style.visibility="hidden";
-    }else{
-    saturday.style.visibility="visible";
-    sunday.style.visibility="visible";
-    }
-  }
+// function hide(){
+//   var checkBox = document.getElementById("weekendYes");
+//
+//   if (checkBox.checked == false){
+//     saturday.style.visibility="hidden";
+//     sunday.style.visibility="hidden";
+//     }else{
+//     saturday.style.visibility="visible";
+//     sunday.style.visibility="visible";
+//     }
+//   }
 
 function calendarCreate(){
   var numberOfWeeks = document.getElementById("noWeeks");
   var firstDate = new Date(document.getElementById("startDate").value);
   var currentDate = new Date(firstDate);
-
-  function makeDay(message) {
-      var day = document.createElement('div');
-      day.classList.add("day");
-      day.innerHTML = message;
-      return day;
-  }
-
-  function makeDivider() {
-      var day = document.createElement('div');
-      day.classList.add("divider");
-      return day;
-  }
+  var checkBox = document.getElementById("weekendYes");
 
   for (var i = 0; i < numberOfWeeks.value*7; i++) {
+
     if (i % 7 === 0) {
-      document.getElementsByTagName('body')[0].appendChild(makeDivider())
-    }
+      document.getElementsByTagName('body')[0].appendChild(makeDivider());
+    };
+
+    if ((i+2) % 7 === 0 && checkBox.checked == false){
+      document.getElementsByTagName('body')[0].appendChild(hiddenDay());
+    }else if((i+1) % 7 == 0 && checkBox.checked == false){
+      document.getElementsByTagName('body')[0].appendChild(hiddenDay());
+    }else{
     currentDate.setDate(firstDate.getDate() + i);
     document.getElementsByTagName('body')[0].appendChild(makeDay(currentDate.toLocaleDateString('en-GB')))
+    }
   }
+}
+
+function makeDay(message) {
+    var day = document.createElement('div');
+    day.classList.add("day");
+    day.innerHTML = message;
+    return day;
+}
+
+function hiddenDay(message) {
+  var day = document.createElement('div');
+  day.classList.add("day");
+  day.classList.add("hidden");
+  day.innerHTML = message;
+  return day;
+}
+
+function makeDivider() {
+    var day = document.createElement('div');
+    day.classList.add("divider");
+    return day;
 }
