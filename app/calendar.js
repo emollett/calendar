76ad6a8c2084b56ce0bd6cc8167913bf47c0c593
Eleanor Mollett <eleanor.mollett@gmail.com>
2018-09-28 17:55:40 +0100

    When you select a date, the calendar will start on the Monday date regardless of what date you selected and work out what the date of the Monday should be.

--- app/calendar.js
+++ app/calendar.js
@@ -2,15 +2,16 @@
 function calendarCreate(){
   var numberOfWeeks = document.getElementById("noWeeks");
   var firstDate = new Date(document.getElementById("startDate").value);
+  var day = firstDate.getDay() - 1;
   var currentDate = new Date(firstDate);
+  currentDate.setDate(currentDate.getDate() - Number(day));
   var checkBox = document.getElementById("weekendYes");
 
   for (var i = 0; i < numberOfWeeks.value*7; i++) {
-
     if (i % 7 === 0) {
       document.getElementsByTagName('body')[0].appendChild(makeDivider());
     };
-    currentDate.setDate(currentDate.getDate() + 1);
+
     if ((i+2) % 7 === 0 && checkBox.checked == false){
       document.getElementsByTagName('body')[0].appendChild(makeDay("hello", true));
     }else if((i+1) % 7 == 0 && checkBox.checked == false){
@@ -18,6 +19,7 @@ function calendarCreate(){
     }else{
     document.getElementsByTagName('body')[0].appendChild(makeDay(currentDate.toLocaleDateString('en-GB')))
     }
+    currentDate.setDate(currentDate.getDate() + 1);
   }
 }
 
@@ -30,14 +32,6 @@ function makeDay(message, isHidden) {
     }
     return day;
 }
-
-// function hiddenDay(message) {
-//   var day = document.createElement('div');
-//   day.classList.add("day");
-//   day.classList.add("hidden");
-//   day.innerHTML = message;
-//   return day;
-// }
 
 function makeDivider() {
     var day = document.createElement('div');
