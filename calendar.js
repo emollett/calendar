9b92e256d533c17053ad8a268e4f22ae4bf4fb75
Eleanor Mollett <eleanor.mollett@gmail.com>
2018-10-01 15:43:05 +0100

    Days of the week added to the top of the calendar. New div added so that the calendar sites in a div of its own - this allows me to print just that div. The boxes now resize according to the size of the page and are different for a pritned page to fill up all of a printed page. I might revist this to expand it when there are 5 boxes so the 5 fill the width of the page.

--- calendar.js
+++ calendar.js
@@ -6,19 +6,32 @@ function calendarCreate(){
   var currentDate = new Date(firstDate);
   currentDate.setDate(currentDate.getDate() - Number(day));
   var checkBox = document.getElementById("weekendYes");
+  var dayNames = ["Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday", "Sunday"];
+
+  if(checkBox.checked == false){
+    var weekLength = 5
+  }else{var weekLength = 7};
+
+  for (var a = 0; a < weekLength; a++) {
+    if (a == 7) {
+      document.getElementsByTagName('div')[12].appendChild(makeDivider());
+    };
+    document.getElementsByTagName('div')[12].appendChild(makeDayTitles(dayNames[a]));
+  };
 
   for (var i = 0; i < numberOfWeeks.value*7; i++) {
     if (i % 7 === 0) {
-      document.getElementsByTagName('body')[0].appendChild(makeDivider());
+      document.getElementsByTagName('div')[12].appendChild(makeDivider());
     };
 
     if ((i+2) % 7 === 0 && checkBox.checked == false){
-      document.getElementsByTagName('body')[0].appendChild(makeDay("hello", true));
+      document.getElementsByTagName('div')[12].appendChild(makeDay("hello", true));
     }else if((i+1) % 7 == 0 && checkBox.checked == false){
-      document.getElementsByTagName('body')[0].appendChild(makeDay("hello", true))
+      document.getElementsByTagName('div')[12].appendChild(makeDay("hello", true))
     }else{
-    document.getElementsByTagName('body')[0].appendChild(makeDay(currentDate.toLocaleDateString('en-GB')))
+    document.getElementsByTagName('div')[12].appendChild(makeDay(currentDate.toLocaleDateString('en-GB')))
     }
+
     currentDate.setDate(currentDate.getDate() + 1);
   }
 }
@@ -37,4 +50,19 @@ function makeDivider() {
     var day = document.createElement('div');
     day.classList.add("divider");
     return day;
+}
+
+function makeDayTitles(message) {
+    var day = document.createElement('div');
+    day.classList.add("dayTitles");
+    day.innerHTML = message;
+    return day;
+}
+
+function printCalendar(){
+  var printContents = document.getElementById("calendarBox").innerHTML;
+	var originalContents = document.body.innerHTML;
+	document.body.innerHTML = printContents;
+	window.print();
+	document.body.innerHTML = originalContents;
 }
